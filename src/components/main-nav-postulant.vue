bfe2a44bde5754830cd33b1cb351a568e29dc978
Fsnete <43191130+Fsnete@users.noreply.github.com>
2021-09-27 16:59:00 -0500

    feat:"Images and navbars"
    Adding Images and postulant and employer navbar

--- src/components/main-nav-postulant.vue
+++ src/components/main-nav-postulant.vue
@@ -1,13 +1,152 @@
 <template>
+  <div>
+    <v-app-bar app color=#FFFFFF>
+      <!--Drawer Icon-->
+      <v-app-bar-nav-icon align="right" @click.stop="drawer = !drawer" color="white"></v-app-bar-nav-icon>
 
+      <v-spacer></v-spacer>
+        <v-btn icon depressed to="/home_postulant">
+          <v-avatar size="100">
+            <v-img
+                max-height="70"
+                max-width="70"
+                src="../assets/logo.png"></v-img>
+          </v-avatar>
+        </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon depressed to="/patientProfile" >
+        <v-avatar right color=#1955AE size="50">
+          <v-img
+              max-height="50"
+              max-width="50"
+              src="https://imagenes.elpais.com/resizer/scDp4N5i3hPpyTudfS1SiOjR9kA=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/CXO5DJG7RJBMLJGJH3LBMZ3ARI"></v-img>
+        </v-avatar>
+      </v-btn>
+
+      <v-btn class="text_bienvenido" plain> Bienvenido </v-btn>
+      <v-btn dark class="boton_logout" color="#EA4E4E">Log Out</v-btn>
+    </v-app-bar>
+
+
+
+    <!--Drawer despegable-->
+    <v-navigation-drawer color=#1955AE v-model="drawer" absolute temporary>
+      <v-list nav dense>
+
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text_sidebar">Inicio</v-list-item-title>
+
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider
+              inset
+          ></v-divider>
+          <v-list-item>
+            <v-list-item-title class="text_sidebar">Postular</v-list-item-title>
+            <v-divider
+                inset
+            ></v-divider>
+          </v-list-item>
+          <v-divider
+              inset
+          ></v-divider>
+          <v-list-item>
+            <v-list-item-title class="text_sidebar">Mis Postulaciones</v-list-item-title>
+            <v-divider
+                inset
+            ></v-divider>
+          </v-list-item>
+          <v-divider
+              inset
+          ></v-divider>
+          <v-list-item>
+            <v-list-item-title class="text_sidebar">Entrevistas</v-list-item-title>
+            <v-divider
+                inset
+            ></v-divider>
+          </v-list-item>
+          <v-divider
+              inset
+          ></v-divider>
+          <v-list-item>
+            <v-list-item-title class="text_sidebar">Mi Cuenta</v-list-item-title>
+            <v-divider
+                inset
+            ></v-divider>
+          </v-list-item>
+          <v-divider
+              inset
+          ></v-divider>
+          <v-list-item>
+            <v-list-item-title class="text_sidebar">Configuración</v-list-item-title>
+            <v-divider
+                inset
+            ></v-divider>
+          </v-list-item>
+          <v-divider
+              inset
+          ></v-divider>
+          <v-list-item color="#EA4E4E">
+            <v-list-item-title class="text_sidebar">Cerrar Sesión</v-list-item-title>
+            <v-divider
+                inset
+            ></v-divider>
+          </v-list-item>
+          <v-divider
+              inset
+          ></v-divider>
+
+          <v-list-item v-for="item in items" :key="item" link :to=item.route>
+
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              {{ item.text }}
+              <v-divider></v-divider>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
+
 
 <script>
 export default {
-  name: "main-nav-postulant"
+  name: "main-nav-postulant",
+
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+  watch: {
+    group() {
+      this.drawer = false
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
+.boton_logout{
+  font-size: 18px;
+  font-family: "Roboto", sans-serif;
+}
+.text_bienvenido{
+  font-size: 18px;
+  font-family: "Roboto", sans-serif;
+}
 
+.text_sidebar{
+  font-size: 18px;
+  font-family: "Roboto", sans-serif;
+  color: white;
+}
 </style>
